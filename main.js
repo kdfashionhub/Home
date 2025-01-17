cc2648d38700f118d311cf94fea12b7612df3aad
KD fashion <136991516+kdfashionhub@users.noreply.github.com>
2025-01-17 17:46:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,12 @@
 console.log("Welcome to Your Clothing and Shoe Store!");
 
 // JavaScript to toggle the menu
-const menuButton = document.getElementById('menu-button');
-const menu = document.getElementById('menu');
 
-if (menuButton && menu) {
-    menuButton.addEventListener('click', () => {
-        menu.classList.toggle('hidden');
-    });
-}
-
-// Function to redirect to WhatsApp with product details
-function redirectToWhatsApp(productName, productPrice) {
-    const message = `I'm interested in the ${productName} for $${productPrice.toFixed(2)}`;
-    const url = `https://wa.me/919887481882?text=${encodeURIComponent(message)}`;
-    window.location.href = url;
-}
+   document.getElementById('menu-button').addEventListener('click', function() {
+            var menu = document.getElementById('menu');
+            if (menu.classList.contains('hidden')) {
+                menu.classList.remove('hidden');
+            } else {
+                menu.classList.add('hidden');
+            }
+        });
