a3b4c96eb77bf6c603c45336b7b39fa61ab52343
KD fashion <136991516+official-dark-user@users.noreply.github.com>
2025-01-17 13:51:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 // Placeholder for future JavaScript functionality
 console.log("Welcome to Your Clothing and Shoe Store!");
+
+// JavaScript to toggle the menu
+const menuButton = document.getElementById('menu-button');
+const menu = document.getElementById('menu');
+
+if (menuButton && menu) {
+    menuButton.addEventListener('click', () => {
+        menu.classList.toggle('hidden');
+    });
+}
